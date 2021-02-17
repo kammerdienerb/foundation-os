406f105bfc97a7f8ca2cad31544e9475d4127720
Brandon Kammerdiener <kammerdienerb@gmail.com>
2021-02-17 17:54:35 -0500

    fine

--- scripts/mk_deps.sh
+++ scripts/mk_deps.sh
@@ -3,6 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}/..
 
+mkdir -p deps
+
+cd deps
+
+git clone https://github.com/jncronin/mkgpt
 cd deps/mkgpt
 automake --add-missing
 autoreconf
